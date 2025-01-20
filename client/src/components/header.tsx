40c83a7a010d5d461938b510fec8b55ef271adfa
sofibear01 <sofibearzotti01@gmail.com>
2025-01-20 19:40:37 -0300

    correccion estilos header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,17 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
-import { Button } from 'primereact/button';
 import React from 'react';
+import { Button } from 'primereact/button';
 
 const Header: React.FC = () => {
-    return (
-        <header className="custom-blue-bg text-white p-2 flex items-center justify-between w-full fixed top-0 left-0" style={{ height: '40px' }}>
-            <div className="flex items-center">
-                <img src="/dux.png" alt="Dux Logo" className="mr-2" style={{ height: '100%', width: 'auto' }} />
-
-            </div>
-            <Button icon="pi pi-cog" className="p-button-rounded p-button-text p-button-lg text-white" />
-        </header>
-    );
+  return (
+    <header
+      className="custom-blue-bg text-white p-2 flex justify-between items-center w-full fixed top-0 left-0"
+      style={{ height: '40px', zIndex: 1000 }}
+    >
+      <div className="flex items-center h-full">
+        <img src="/dux.png" alt="Dux Logo" className="mr-2" style={{ height: '100%' }} />
+      </div>
+      <div className="flex items-center h-full ml-auto">
+        <Button
+          icon="pi pi-cog"
+          className="p-button-rounded p-button-text p-button-lg text-white"
+          style={{ height: '100%' }}
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
